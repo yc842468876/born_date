67bafc487d1fc485187ccfe4dcbaa67de51ccf66
Felix <yc842468876@163.com>
2025-03-05 14:47:28 +0800

    456

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log('Nikki部Nikki组、Nikki部Josh组');
+const dayjs = require('dayjs');
+const isLeapYear = require('dayjs/plugin/isLeapYear');
+dayjs.extend(isLeapYear);
+const now = dayjs();
+const data = now.isBefore(dayjs('2025-02-28').endOf('day'));
+const data2 = now.subtract(1, 'year').isLeapYear();
+
+console.log(data, data2);
