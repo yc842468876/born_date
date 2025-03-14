2e13f57f740cec12448a304b2c70b95d6cb718ed
Felix <yc842468876@163.com>
2025-03-14 10:02:52 +0800

    bornDate

--- README.md
+++
@@ -1,1 +0,0 @@
-# <center>自述文件</center>
--- test.js
+++
@@ -1,5 +0,0 @@
-const dayjs = require('dayjs');
-const calendar = require('chinese-workday');
-
-const data = dayjs().diff('2025-01-19', 'day');
-console.log(data, data > 7);
